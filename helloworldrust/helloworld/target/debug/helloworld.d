355f9c70bc84c5ac01d7283a129d114ffe032a8c
uplandwave <113386040+uplandwave@users.noreply.github.com>
2024-01-11 12:58:53 -0700

    new video link

--- helloworldrust/helloworld/target/debug/helloworld.d
+++ helloworldrust/helloworld/target/debug/helloworld.d
@@ -1,1 +1,1 @@
-/Users/lukebriggs/Documents/GitHub/310/helloworldrust/helloworld/target/debug/helloworld: /Users/lukebriggs/Documents/GitHub/310/helloworldrust/helloworld/src/main.rs
+/Users/lukebriggs/Documents/GitHub/helloworld-rust/helloworldrust/helloworld/target/debug/helloworld: /Users/lukebriggs/Documents/GitHub/helloworld-rust/helloworldrust/helloworld/src/main.rs
